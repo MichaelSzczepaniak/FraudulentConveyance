8e6b55f148fbbd2c4c9a61cc766818f7031999f7
MichaelSzczepaniak <mszczepaniak@pobox.com>
2016-08-02 18:10:13 -0600

    MerchantDropData.csv - delimit column headers with underscores instead of periods OsMerchantParser.py - init port of java page parser to Python

---
+++ code/OsMerchantParser.py
@@ -0,0 +1,18 @@
+#
+#
+#
+
+class OsMerchantParser:
+    # define some constants
+    toToken = "To";
+    contactToken = "Contact:";
+    phoneToken = "Phone:";
+    faxToken = "Fax:";
+    timeZoneToken = "Time Zone:";
+    merchantToken = "Merchant:";
+    siteToken = "Site:";
+    terminalToken = "Terminal:";
+    Date = "Date";
+
+    __init__(self):
+        print("initializing OsMerchantParser...")
